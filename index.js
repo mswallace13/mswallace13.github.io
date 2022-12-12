a5f8887331010482e5c0f466173f0797f837faad
EDU_22 <79609321+mswallace13@users.noreply.github.com>
2022-12-12 11:27:51 -0600

    mlb

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// define variables for the operands and the result
-let x = prompt("num 1");
-let y = prompt("num 2");
-var result = 0;
+// First, we need to import the 'fetch' library, which allows us to make
+// HTTP requests to the API
+import fetch from 'fetch';
 
-// define a function to add the operands
-function addition(x, y) {
-  return +x + +y;
+// Next, we need to define the URL of the API endpoint that we want to
+// retrieve data from. In this case, it's the endpoint for the MLB schedule
+const API_URL = 'https://mlb-schedule-api.com/schedule';
+
+// Now we can create a function that retrieves the data from the API
+async function getMLBGames() {
+  // Use the 'fetch' library to make a GET request to the API
+  const response = await fetch(API_URL);
+
+  // Convert the response to JSON format
+  const data = await response.json();
+
+  // Return the data to the caller of this function
+  return data;
 }
 
-// define a function to subtract the operands
-function subtract(x, y) {
-  return x - y;
-}
+// Now that we have our function, we can call it and use the data it returns
+// to populate our website with information about the upcoming games
+getMLBGames().then(games => {
+  // Loop through the games and display their details on the website
+  games.forEach(game => {
+    // Create a new element to hold the game details
+    const gameElement = document.createElement('div');
 
-// define a function to multiply the operands
-function multiply(x, y) {
-  return x * y;
-}
+    // Add the game details to the element
+    gameElement.innerHTML = `
+      <h2>${game.teams.home.name} vs ${game.teams.away.name}</h2>
+      <p>Date: ${game.date}</p>
+      <p>Time: ${game.time}</p>
+      <p>Stadium: ${game.location}</p>
+    `;
 
-// define a function to divide the operands
-function divide(x, y) {
-  return x / y;
-}
-
-// perform the addition and store the result
-result = addition(x, y);
-prompt(x + " + " + y + " = " + result);
-
-// perform the subtraction and store the result
-result = subtract(x, y);
-prompt(x + " - " + y + " = " + result);
-
-// perform the multiplication and store the result
-result = multiply(x, y);
-prompt(x + " * " + y + " = " + result);
-
-// perform the division and store the result
-result = divide(x, y);
-prompt(x + " / " + y + " = " + result);
+    // Add the game element to the website
+    document.body.appendChild(gameElement);
+  });
+});
