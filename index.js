6ad1cffe091a1b81608e4d66db35d7ddba20de06
EDU_22 <79609321+mswallace13@users.noreply.github.com>
2022-12-12 10:51:37 -0600

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+// define variables for the operands and the result
+let x = prompt("num 1");
+let y = prompt("num 2");
+let result = 0;
+
+// define a function to add the operands
+function add(x, y) {
+  return x + y;
+}
+
+// define a function to subtract the operands
+function subtract(x, y) {
+  return x - y;
+}
+
+// define a function to multiply the operands
+function multiply(x, y) {
+  return x * y;
+}
+
+// define a function to divide the operands
+function divide(x, y) {
+  return x / y;
+}
+
+// perform the addition and store the result
+result = add(x, y);
+prompt(x + " + " + y + " = " + result);
+
+// perform the subtraction and store the result
+result = subtract(x, y);
+prompt(x + " - " + y + " = " + result);
+
+// perform the multiplication and store the result
+result = multiply(x, y);
+prompt(x + " * " + y + " = " + result);
+
+// perform the division and store the result
+result = divide(x, y);
+prompt(x + " / " + y + " = " + result);
